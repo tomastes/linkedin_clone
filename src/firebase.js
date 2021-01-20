f0b58c420f5bd44fee61fe83734d8ca104e6f03b
tomas <tomasberhe1999@gmail.com>
2021-01-20 18:26:39 +0100

    bug fixed

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,7 @@
 import react from 'react'
 import firebase from 'firebase'
 const firebaseConfig = {
-    apiKey: "AIzaSyBNNW9RiEc1H7QcndxiGDiPLLnp3fxaao0",
-    authDomain: "linkedin-clone-e9098.firebaseapp.com",
-    projectId: "linkedin-clone-e9098",
-    storageBucket: "linkedin-clone-e9098.appspot.com",
-    messagingSenderId: "533917479248",
-    appId: "1:533917479248:web:925763e282f97d9905bc55"
+  
   };
   const firebaseApp = firebase.initializeApp(firebaseConfig)
   const db = firebaseApp.firestore()
